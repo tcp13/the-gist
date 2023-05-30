4174091ae8381110001ef6377c09b5d8b00b1af4
Tyler Puckett <16280707+tcp13@users.noreply.github.com>
2023-05-29 20:52:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+var news = [];
+
+$.getJSON("news.json")
+    .done(function(data){
+        news = data;
+        populateNews();
+    })
+    .fail(function(){
+        console.log("Hmm, something went wrong. Unable to retrieve news.");
+    });
+
+
+
+
+function populateNews(){
+    for(i=0; i<10; i++){
+        let sources = "";
+    
+        for(j=0; j<news[i].sources.length; j++){
+            let url = new URL(news[i].sources[j]);
+            sources += `<a href='${url}' target='_blank'><span class="source tooltip">...<span class="tooltip-text">${url.host}</span></span></a>`;
+        }
+        
+        $("main").append(`<div class="card"><h2 class="date">${news[i].date}</h2>${sources}<p>${news[i].content}</p></div>`);
+    }
+}
+
+
+
+/* DARK MODE */
+if(window.localStorage.getItem("darkMode") != null && window.localStorage.getItem("darkMode") == false){
+    $("body").removeClass("dark-mode");
+    $("#socialShare img").css("filter", "brightness(0.1)");
+    $("#toggle-dark-mode").text("Dark Mode");
+}
+function toggleDarkMode(){
+    if($("body").hasClass("dark-mode")){
+        $("body").removeClass("dark-mode");
+        $("#socialShare img").css("filter", "brightness(0.1)");
+        $("#toggle-dark-mode").text("Dark Mode");
+        window.localStorage.setItem("darkMode", false);
+    }
+    else{
+        $("body").addClass("dark-mode");
+        $("#socialShare img").css("filter", "brightness(1)");
+        $("#toggle-dark-mode").text("Light Mode");
+        window.localStorage.setItem("darkMode", true);
+    }
+}
