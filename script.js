b1ee5ca8ed3e7caeb4e004f8bebe0f4002d94261
Tyler Puckett <16280707+tcp13@users.noreply.github.com>
2023-06-02 15:38:40 -0400

    add thumbnail, seo data, favicon, and about page

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function populateNews(){
             sources += `<a href='${url}' target='_blank'><span class="source tooltip">...<span class="tooltip-text">${url.host}</span></span></a>`;
         }
         
-        $("main").append(`<div class="card"><h2 class="date">${news[i].date}</h2>${sources}<p>${news[i].content}</p></div>`);
+        $("#news-feed").append(`<div class="card"><h2 class="date">${news[i].date}</h2>${sources}<p>${news[i].content}</p></div>`);
     }
 }
 
